b0587dfeeba0378bbdba14f9a21a70e726a87595
amr hany <amrhanyseed@gmail.com>
2024-08-20 21:49:44 +0300

    version 1

---
+++ mian.js
@@ -0,0 +1,35 @@
+let dark_btn=document.querySelector(".Dark");
+let x=dark_btn.innerHTML;
+///<i class='bx bx-sun'></i>
+const root = document.querySelector(':root');
+
+let flag=false
+
+dark_btn.addEventListener("click",function(){
+document.body.classList.toggle("dark");
+if(flag==false){
+    dark_btn.innerHTML="";
+    dark_btn.innerHTML=`<i class='bx bx-sun'></i> Light Mode`;
+root.style.setProperty('--text-main-color',"#ffffff");
+root.style.setProperty('--main--bg-color',"#0000");
+root.style.setProperty('--card-bg-color',"#333");
+flag=true;
+console.log("from black to light")
+}
+else {
+    dark_btn.innerHTML="";
+    dark_btn.innerHTML=x;
+    root.style.setProperty('--text-main-color',"#333");
+    root.style.setProperty('--card-bg-color',"#fff");
+    root.style.setProperty('--main--bg-color',"#f1f1f1");
+    flag=false;
+}
+})
+
+
+
+
+
+// Access the CSS variable value
+
+
